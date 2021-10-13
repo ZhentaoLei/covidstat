48e1faa3c2ccf0e98bb71e1fac5e41f68cf975c3
Zhentao <zhentao@ualberta.ca>
2021-10-12 19:15:42 -0600

    undo stash...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Cards from "./components/Cards/Cards";
+import Charts from "./components/Charts/Charts";
+import Region from "./components/Region/Region";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import styles from "./App.module.css";
+import fetchData from "./api/index";
+class App extends React.Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const data = await fetchData();
+    this.setState({ data: data });
+  }
+  render() {
+    const { data } = this.state;
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <Charts />
+        <Region />
+      </div>
+    );
+  }
 }
 
 export default App;
